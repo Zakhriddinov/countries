2e5f4fa6409773a45b2ade634c1f44eb09fb0b00
Zakhriddinov <zaxriddinovsardor21@gmail.com>
2023-02-17 16:49:20 +0500

    ui completed

--- src/components/CountriesCard.tsx
+++ src/components/CountriesCard.tsx
@@ -1,41 +1,62 @@
+import PhoneIcon from "@mui/icons-material/Phone";
+import TranslateIcon from "@mui/icons-material/Translate";
+import LanguageIcon from "@mui/icons-material/Language";
 import { CardContent } from "@mui/material";
 import { Box } from "@mui/material";
-import { Button } from "@mui/material";
-import { CardActions } from "@mui/material";
 import { Typography } from "@mui/material";
 import { Card } from "@mui/material";
 import React from "react";
+import "./style.css";
+import { CardMedia } from "@mui/material";
 
 const CountriesCard = () => {
-  const bull = (
-    <Box
-      component="span"
-      sx={{ display: "inline-block", mx: "2px", transform: "scale(0.8)" }}
-    >
-      •
-    </Box>
-  );
+  const style = {
+    display: "flex",
+    gap: "5px",
+  } as const;
   return (
-    <Card sx={{ minWidth: 275 }}>
+    <Card sx={{ width: 400, borderRadius: "20px", border: "1px solid black" }}>
       <CardContent>
-        <Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>
-          Word of the Day
-        </Typography>
-        <Typography variant="h5" component="div">
-          be{bull}nev{bull}o{bull}lent
-        </Typography>
-        <Typography sx={{ mb: 1.5 }} color="text.secondary">
-          adjective
-        </Typography>
-        <Typography variant="body2">
-          well meaning and kindly.
-          <br />
-          {'"a benevolent smile"'}
-        </Typography>
+        <Box
+          sx={{
+            display: "flex",
+            justifyContent: "space-between",
+            alignItems: "flex-start",
+            marginBottom: "15px",
+          }}
+        >
+          <div style={{ display: "flex", gap: "10px", alignItems: "center" }}>
+            <CardMedia
+              component="img"
+              alt="green iguana"
+              height="40"
+              width={"40"}
+              image="https://upload.wikimedia.org/wikipedia/commons/thumb/8/84/Flag_of_Uzbekistan.svg/1200px-Flag_of_Uzbekistan.svg.png"
+            />
+            <div>
+              <Typography variant="h6" component={"h5"}>
+                Uzbekistan
+              </Typography>
+              <Typography variant="body1">Tashkent</Typography>
+            </div>
+          </div>
+          <Typography variant="body1">Asia</Typography>
+        </Box>
+        <div className="bottom-action">
+          <Box sx={style}>
+            <PhoneIcon sx={{ maxWidth: "18px" }} />
+            <Typography sx={{ fontSize: "14px" }}>998</Typography>
+          </Box>
+          <Box sx={style}>
+            <TranslateIcon sx={{ maxWidth: "18px" }} />
+            <Typography sx={{ fontSize: "14px" }}>UZS</Typography>
+          </Box>
+          <Box sx={style}>
+            <LanguageIcon sx={{ maxWidth: "18px" }} />
+            <Typography sx={{ fontSize: "14px" }}>Uzbek, Russia</Typography>
+          </Box>
+        </div>
       </CardContent>
-      <CardActions>
-        <Button size="small">Learn More</Button>
-      </CardActions>
     </Card>
   );
 };
